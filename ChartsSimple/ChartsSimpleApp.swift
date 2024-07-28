21251bb9c2640f93d45dc9f67b7ff614689c88f8
Chris Gonzalez <christogonz92@gmail.com>
2024-07-28 15:52:40 +0200

    Initial Commit

---
+++ ChartsSimple/ChartsSimpleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChartsSimpleApp.swift
+//  ChartsSimple
+//
+//  Created by Christopher Gonzalez on 2024-07-28.
+//
+
+import SwiftUI
+
+@main
+struct ChartsSimpleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
